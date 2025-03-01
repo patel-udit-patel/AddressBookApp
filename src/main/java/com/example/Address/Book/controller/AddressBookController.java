046231e7f489c205d0dc7c3599de82933f0dea3f
patel-udit-patel <udit0428t@gmail.com>
2025-03-01 13:45:24 +0530

    [Udit Patel] Add .Introducing DTO and Model to AddressBook App

--- src/main/java/com/example/Address/Book/controller/AddressBookController.java
+++ src/main/java/com/example/Address/Book/controller/AddressBookController.java
@@ -1,11 +1,10 @@
 package com.example.Address.Book.controller;
 
-
-import com.example.Address.Book.AddressBookApplication;
 import com.example.Address.Book.DTO.AddressBookDTO;
 import com.example.Address.Book.repository.AddressBookRepository;
 import com.example.Address.Book.model.AddressBookEntry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,17 +23,17 @@ public class AddressBookController {
     @Autowired
     private AddressBookRepository repository;
 
-    @GetMapping("/all")
+    @GetMapping("/api/all")
     public List<AddressBookEntry>getAllInfo(){
         return repository.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/{id}")
     public AddressBookEntry getInfoById(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new RuntimeException("Entry not found"));
     }
 
-    @PostMapping
+    @PostMapping("/api")
     public AddressBookEntry addInfo(@RequestBody AddressBookDTO dataDTO){
         AddressBookEntry data = new AddressBookEntry();
         data.setName(dataDTO.getName());
@@ -46,7 +45,7 @@ public class AddressBookController {
         return data;
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/api/{id}")
     public AddressBookEntry updateInfo(@PathVariable Long id,@RequestBody AddressBookEntry dataDTO){
         AddressBookEntry data = new AddressBookEntry();
         data.setId(id);
@@ -58,10 +57,56 @@ public class AddressBookController {
         return data;
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/api/{id}")
     public AddressBookEntry deleteInfo(@PathVariable Long id){
         AddressBookEntry data=repository.findById(id).orElseThrow(() -> new RuntimeException("Entry not found"));
          repository.delete(data);
          return data;
     }
+    //UC01_section02
+
+    @GetMapping("/all")
+    public ResponseEntity<List<AddressBookEntry>>getAllInfoResponse(){
+        List<AddressBookEntry> entry = repository.findAll();
+        return ResponseEntity.ok(entry);
+        }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AddressBookEntry> getInfoByIdResponse(@PathVariable Long id) {
+        AddressBookEntry entry= repository.findById(id).orElseThrow(() -> new RuntimeException("Entry not found"));
+        return ResponseEntity.ok(entry);
+    }
+
+    @PostMapping
+    public ResponseEntity<AddressBookEntry> addInfoResponse(@RequestBody AddressBookDTO dataDTO){
+        AddressBookEntry data = new AddressBookEntry();
+        data.setName(dataDTO.getName());
+        data.setContact(dataDTO.getContact());
+        data.setEmail(dataDTO.getEmail());
+
+        repository.save(data);
+
+        return ResponseEntity.ok(data);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AddressBookEntry> updateInfoResponse(@PathVariable Long id,@RequestBody AddressBookEntry dataDTO){
+        AddressBookEntry data = new AddressBookEntry();
+        data.setId(id);
+        data.setName(dataDTO.getName());
+        data.setContact(dataDTO.getContact());
+        data.setEmail(dataDTO.getEmail());
+
+        repository.save(data);
+        return ResponseEntity.ok(data);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AddressBookEntry> deleteInfoResponse(@PathVariable Long id){
+        AddressBookEntry data=repository.findById(id).orElseThrow(() -> new RuntimeException("Entry not found"));
+        repository.delete(data);
+        return ResponseEntity.ok(data);
+    }
+
+
 }
